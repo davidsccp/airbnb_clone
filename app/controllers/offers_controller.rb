95912bb67cdd6b47f9051e49087cd1a336f019a4
Alexandre Simoes <lexsimoes@yahoo.com>
2022-05-12 20:20:44 -0300

    controllers

---
+++ app/controllers/offers_controller.rb
@@ -0,0 +1,81 @@
+class OffersController < ApplicationController
+  before_action :set_offer, only: [:show, :edit, :destroy, :update]
+  before_action :authenticate_user!, except: [:index]
+
+  def index
+    @offers = current_user.offers.all
+  end
+
+  def new
+    @offer = current_user.offers.build
+  end
+
+  def create
+    @offer = current_user.offers.build(offer_params)
+    if @offer.save
+      flash[:notice] = "Your offer was successfully created"
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def edit
+    if current_user.id != @offer.user.id
+      redirect_to root_path
+      flash[:alert] = "You don't have permission"
+    end
+  end
+
+  def update
+    urls = JSON.parse params[:offer][:urls]
+    urls.each do |url|
+      if !@offer.urls.include?(url)
+        @offer.urls << url
+      end
+    end
+    if @offer.update(offer_params)
+      redirect_to edit_offer_path(@offer)
+      flash[:notice] = "Your offer was updated successfully..."
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @offer.destroy
+    flash[:notice] = "Your offer was removed successfully"
+    redirect_to offers_path
+  end
+
+  def delete_images
+    @offer = Offer.find(params[:room_id])
+    if current_user.id == @offer.user.id
+      index = params[:image_id]
+      @offer.urls.delete_at(index.to_i)
+      @offer.save
+      respond_to :js
+    else
+      flash[:alert] = "You don't have permission"
+      redirect_to root_path
+    end
+  end
+
+  private
+  def set_offer
+    @offer = Offer.find(params[:id])
+  end
+
+  def offer_params
+    params[:offer][:urls] ||= []
+    params.require(:room).permit(
+      :home_type, :room_type, :accomdate,
+      :bedroom, :bathroom, :listing_name,:city,
+      :state, :summary, :address, :is_tv, :is_kitchen,
+      :is_air, :is_heating, :is_internet, :price, urls: []
+    )
+  end
+end
---
+++ app/controllers/transaction_controller.rb
@@ -0,0 +1,3 @@
+class TransactionController < ApplicationController
+  before_action :authenticate_user!
+end
