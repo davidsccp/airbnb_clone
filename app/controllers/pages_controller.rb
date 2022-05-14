38d65539e67f469aa653c744ebbbd76313e2fc5b
Alexandre Simoes <lexsimoes@yahoo.com>
2022-05-14 16:41:46 -0300

    views done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,13 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :home
 
-  def index
+  def home
   end
 
   def search
     @address = params[:address]
+    @state = params[:state]
+    @city = params[:city]
     @checkin = params[:checkin]
     @checkout = params[:checkout]
     @guests = params[:guests] || 1
